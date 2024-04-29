bc87dea7c41dfb55429853e27cbd8eed2df5754e
Deonte Kilgore <deonte.kilgore.dev@gmail.com>
2024-04-29 10:25:20 -0400

    - Created base calendar view - Updated persistence class

--- CalendarStudy/ContentView.swift
+++ CalendarStudy/ContentView.swift
@@ -15,15 +15,35 @@ struct ContentView: View {
         sortDescriptors: [NSSortDescriptor(keyPath: \Day.date, ascending: true)],
         animation: .default)
     private var days: FetchedResults<Day>
-
+    
+    let daysOfWeek = ["Su", "M", "T", "W", "Th", "F", "S"]
     var body: some View {
         NavigationView {
-            List {
-                ForEach(days) { day in
-                    Text("Item at \(day.date!.formatted())")
+            VStack {
+                HStack {
+                    ForEach(daysOfWeek, id: \.self) { dayOfWeek in
+                        Text(dayOfWeek)
+                            .fontWeight(.black)
+                            .foregroundStyle(Color.green)
+                            .frame(maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/)
+                    }
                 }
+                LazyVGrid(columns: Array(repeating: GridItem(.flexible()), count: 7)) {
+                    ForEach(days) { day in
+                        Text(day.date!.formatted(.dateTime.day()))
+                            .fontWeight(.bold)
+                            .foregroundStyle(day.didStudy ? .green : .secondary)
+                            .frame(maxWidth: .infinity, minHeight: 40)
+                            .background(
+                                Circle()
+                                    .foregroundStyle(Color.green.opacity(day.didStudy ? 0.3 : 0.0))
+                            )
+                    }
+                }
+                Spacer()
             }
-            
+            .navigationTitle(Date().formatted(.dateTime.month(.wide)))
+            .padding()
         }
     }
     
