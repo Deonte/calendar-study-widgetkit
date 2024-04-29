375f3e9eb761102d05101a0d5f91cc4fa2577b88
Deonte Kilgore <deonte.kilgore.dev@gmail.com>
2024-04-29 12:34:15 -0400

    - Added widgetKit extension - handle core data migration

---
+++ CalendarStudyWidget/AppIntent.swift
@@ -0,0 +1,18 @@
+//
+//  AppIntent.swift
+//  CalendarStudyWidget
+//
+//  Created by Deonte Kilgore on 4/29/24.
+//
+
+import WidgetKit
+import AppIntents
+
+struct ConfigurationAppIntent: WidgetConfigurationIntent {
+    static var title: LocalizedStringResource = "Configuration"
+    static var description = IntentDescription("This is an example widget.")
+
+    // An example configurable parameter.
+    @Parameter(title: "Favorite Emoji", default: "😃")
+    var favoriteEmoji: String
+}
---
+++ CalendarStudyWidget/CalendarStudyWidgetBundle.swift
@@ -0,0 +1,16 @@
+//
+//  CalendarStudyWidgetBundle.swift
+//  CalendarStudyWidget
+//
+//  Created by Deonte Kilgore on 4/29/24.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct CalendarStudyWidgetBundle: WidgetBundle {
+    var body: some Widget {
+        CalendarStudyWidget()
+    }
+}
